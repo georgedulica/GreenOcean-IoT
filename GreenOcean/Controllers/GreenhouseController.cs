6c8ca7608c7f4961303a2ae11116cd12723993cd
george.dulica <dulica_george_10@yahoo.com>
2024-03-04 08:25:56 +0200

    Backend for Greenhouse

--- GreenOcean/Controllers/GreenhouseController.cs
+++ GreenOcean/Controllers/GreenhouseController.cs
@@ -4,6 +4,7 @@ using GreenOcean.DTOs;
 using GreenOcean.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace GreenOcean.Controllers;
 
@@ -20,7 +21,146 @@ public class GreenhouseController : ControllerBase
         this.mapper = mapper;
     }
 
-    [HttpPut("/creategreenhouse/{username}")]
+    [HttpGet("{username}")]
+    public async Task<ActionResult<IEnumerable<GreenhouseDTO>>> GetGreenhouses(string username)
+    {
+        var user = await dataContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Username, username));
+        if (user == null)
+        {
+            return BadRequest();
+        }
+
+        var greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).ToListAsync();
+        var greenhousesAsDTO = mapper.Map<IEnumerable<GreenhouseDTO>>(greenhouses);
+
+        return Ok(greenhousesAsDTO);
+    }
+
+    //[HttpGet("edit/{username}/{id}")]
+    //public async Task<ActionResult<GreenhouseDTO>> GetGreenhouse(string username, string id)
+    //{
+    //    var user = await dataContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Username, username));
+    //    if (user == null)
+    //    {
+    //        return BadRequest();
+    //    }
+
+    //    if (!Guid.TryParse(id, out Guid greenhouseId))
+    //    {
+    //        return BadRequest("Invalid id format");
+    //    }
+
+    //    var greenhouse = await dataContext.Greenhouses.FirstOrDefaultAsync(g => g.Id == greenhouseId && g.UserId == user.Id);
+    //    var greenhouseAsDTO = mapper.Map<IEnumerable<GreenhouseDTO>>(greenhouse);
+
+    //    return Ok(greenhouseAsDTO);
+    //}
+
+    [HttpGet("orderbyname/{username}/{type}")]
+    public async Task<ActionResult<IEnumerable<GreenhouseDTO>>> OrderByName(string username, string type)
+    {
+        var user = await dataContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Username, username));
+        if (user == null)
+        {
+            return BadRequest();
+        }
+
+        IEnumerable<Greenhouse> greenhouses = new List<Greenhouse>();
+
+        if (string.Equals(type, "ascendent"))
+        {
+            greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).OrderBy(g => g.Name).ToListAsync();
+        }
+
+        if (string.Equals(type, "descendent"))
+        {
+            greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).OrderByDescending(g => g.Name).ToListAsync();
+        }
+        
+        var greenhousesAsDTO = mapper.Map<IEnumerable<GreenhouseDTO>>(greenhouses);
+
+        return Ok(greenhousesAsDTO);
+    }    
+    
+    [HttpGet("orderbystreet/{username}/{type}")]
+    public async Task<ActionResult<IEnumerable<GreenhouseDTO>>> OrderByStreet(string username, string type)
+    {
+        var user = await dataContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Username, username));
+        if (user == null)
+        {
+            return BadRequest();
+        }
+
+        IEnumerable<Greenhouse> greenhouses = new List<Greenhouse>();
+
+        if (string.Equals(type, "ascendent"))
+        {
+            greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).OrderBy(g => g.Street).ToListAsync();
+        }
+
+        if (string.Equals(type, "descendent"))
+        {
+            greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).OrderByDescending(g => g.Street).ToListAsync();
+        }
+        
+        var greenhousesAsDTO = mapper.Map<IEnumerable<GreenhouseDTO>>(greenhouses);
+
+        return Ok(greenhousesAsDTO);
+    }    
+    
+    [HttpGet("orderbynumber/{username}/{type}")]
+    public async Task<ActionResult<IEnumerable<GreenhouseDTO>>> OrderByNumber(string username, string type)
+    {
+        var user = await dataContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Username, username));
+        if (user == null)
+        {
+            return BadRequest();
+        }
+
+        IEnumerable<Greenhouse> greenhouses = new List<Greenhouse>();
+
+        if (string.Equals(type, "ascendent"))
+        {
+            greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).OrderBy(g => g.Number).ToListAsync();
+        }
+
+        if (string.Equals(type, "descendent"))
+        {
+            greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).OrderByDescending(g => g.Number).ToListAsync();
+        }
+        
+        var greenhousesAsDTO = mapper.Map<IEnumerable<GreenhouseDTO>>(greenhouses);
+
+        return Ok(greenhousesAsDTO);
+    }    
+    
+    [HttpGet("orderbycity/{username}/{type}")]
+    public async Task<ActionResult<IEnumerable<GreenhouseDTO>>> OrderByCity(string username, string type)
+    {
+        var user = await dataContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Username, username));
+        if (user == null)
+        {
+            return BadRequest();
+        }
+
+        IEnumerable<Greenhouse> greenhouses = new List<Greenhouse>();
+
+        if (string.Equals(type, "ascendent"))
+        {
+            greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).OrderBy(g => g.City).ToListAsync();
+        }
+
+        if (string.Equals(type, "descendent"))
+        {
+            greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).OrderByDescending(g => g.City).ToListAsync();
+        }
+        
+        var greenhousesAsDTO = mapper.Map<IEnumerable<GreenhouseDTO>>(greenhouses);
+
+        return Ok(greenhousesAsDTO);
+    }
+
+    [HttpPost("creategreenhouse/{username}")]
     public async Task<IActionResult> CreateGreenhouse(GreenhouseDTO greenhouseDTO, string username)
     {
         if (greenhouseDTO.Name == null)
@@ -48,9 +188,9 @@ public class GreenhouseController : ControllerBase
 
         return Ok();
     }
-
-    [HttpGet("{username}")]
-    public async Task<ActionResult<IEnumerable<GreenhouseDTO>>> GetGreenHouse(string username)
+    
+    [HttpPut("editgreenhouse/{username}/{id}")]
+    public async Task<ActionResult> EditGreenhouse(GreenhouseDTO greenhouseDTO, string username, string id)
     {
         var user = await dataContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Username, username));
         if (user == null)
@@ -58,9 +198,55 @@ public class GreenhouseController : ControllerBase
             return BadRequest();
         }
 
-        var greenhouses = await dataContext.Greenhouses.Where(g => g.UserId == user.Id).ToListAsync();
-        var greenhousesAsDTO = mapper.Map<IEnumerable<GreenhouseDTO>>(greenhouses);
+        if (!Guid.TryParse(id, out Guid greenhouseId))
+        {
+            return BadRequest("Invalid id format");
+        }
 
-        return Ok(greenhousesAsDTO);
+        if (greenhouseDTO.Name == null)
+        {
+            return BadRequest();
+        }
+        
+        var greenhouse = await dataContext.Greenhouses.FirstOrDefaultAsync(g => g.Id == greenhouseId && g.UserId == user.Id);
+        if (greenhouse == null)
+        {
+            return BadRequest();
+        }
+
+        greenhouse.Name = greenhouseDTO.Name;
+        greenhouse.Street = greenhouseDTO.Street;
+        greenhouse.Street = greenhouseDTO.Street;
+        greenhouse.City = greenhouseDTO.City;
+
+        await dataContext.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    [HttpDelete("deletegreenhouse/{username}/{id}")]
+    public async Task<ActionResult> DeleteGreenhouse(string username, string id)
+    {
+        var user = await dataContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Username, username));
+        if (user == null)
+        {
+            return BadRequest();
+        }
+
+        if (!Guid.TryParse(id, out Guid greenhouseId))
+        {
+            return BadRequest("Invalid id format");
+        }
+
+        var greenhouse = await dataContext.Greenhouses.FirstOrDefaultAsync(g => g.Id == greenhouseId && g.UserId == user.Id);
+        if (greenhouse == null)
+        {
+            return BadRequest();
+        }
+
+        dataContext.Remove(greenhouse);
+        await dataContext.SaveChangesAsync();
+
+        return Ok();
     }
 }
